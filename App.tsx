4171a2a1d363910ca5df47ac2980b95d0117a1d8
luisantonioj <luisantoniojopia@gmail.com>
2026-01-21 18:57:57 +0800

    feat(main): explore basic components

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image 
+        source={require('./assets/splash-icon.png')}
+        style={{ width: 200, height: 200 }}
+      />
+      <Text>Luis Antonio M. Jopia</Text>
+      <Text>Web and Mobile Developer</Text>
+
       <StatusBar style="auto" />
     </View>
   );
